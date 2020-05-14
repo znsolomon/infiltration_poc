880e8208c109d3d73dc0bce056399faa5cff3182
znsolomon <znsolomon@gmail.com>
2020-05-14 13:25:07 +0100

    Setting up enemy logic (STAGE 3)
    -Player now gets 3 moves before turn passes to
    enemy, this is communicated via text
    -Spaces the player moves through are recorded
    -Start of enemy logic added:
     -Enemy now has an enum denoting logic, although
      this is currently just 'RUSH', directly heading
      to target
     -Method added which checks all spaces each enemy
      can see in turn

--- aitest/Main.java
+++ aitest/Main.java
@@ -9,17 +9,31 @@ public class Main {
 			new Coordinate(4,2),new Coordinate(5,2),new Coordinate(6,2),new Coordinate(1,5),new Coordinate(2,5),
 			new Coordinate(3,5),new Coordinate(4,5),new Coordinate(5,5),new Coordinate(6,5)};
 	public static boolean running;
+	public static boolean pTurn;
+	public static int pMoves;
 	
 	public static void main(String[] args) {
 		running = true;
+		pTurn = true;
+		pMoves = 3;
 		Scanner input = new Scanner(System.in);
 		Map levelOne = new Map(8,8, new Coordinate(0,0), new Coordinate[] {new Coordinate(7,0), new Coordinate(7,7)}, walls);
 		print2dArray(levelOne.getGrid());
 		while (running) {
-			System.out.println("Player move (WASD)");
-			String move = input.nextLine();
-			levelOne.playerMove(move);
-			print2dArray(levelOne.getGrid());
+			System.out.println("Player turn");
+			while(pTurn) {
+				System.out.println("Moves left: " + Integer.toString(pMoves));
+				System.out.println("Player move (WASD)");
+				String move = input.nextLine();
+				levelOne.playerMove(move);
+				print2dArray(levelOne.getGrid());
+				pMoves--;
+				if(pMoves <= 0) {
+					pTurn = false;
+				}
+			}
+			System.out.println("Enemy turn");
+			running = false;
 		}
 	}
 
