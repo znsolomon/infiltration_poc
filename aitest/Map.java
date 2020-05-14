30ed6e42db784cfcc0e54e06951a2905bfe824fa
znsolomon <znsolomon@gmail.com>
2020-05-14 13:25:07 +0100

    Setting up enemy logic (STAGE 3)
    -Player now gets 3 moves before turn passes to
    enemy, this is communicated via text
    -Spaces the player moves through are recorded
    -Start of enemy logic added:
     -Enemy now has an enum denoting logic, although
      this is currently just 'RUSH', directly heading
      to target
     -Method added which checks all spaces each enemy
      can see in turn

--- aitest/Map.java
+++ aitest/Map.java
@@ -1,5 +1,6 @@
 package aitest;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import aitest.actors.*;
@@ -8,6 +9,8 @@ import library.Physics;
 public class Map {
 	public HashMap<String,Actor> objects = new HashMap<String,Actor>();
 	public static final String empty = "_____";
+	public ArrayList<String> eKeys = new ArrayList<String>();
+	public ArrayList<int[]> pVisited = new ArrayList<int[]>();
 	public String[][] grid;
 	public Map(int width, int height, Coordinate p, Coordinate[] e, Coordinate[] walls){
 		grid = new String[width][height];
@@ -21,8 +24,9 @@ public class Map {
 		int eCount = 0;
 		for(Coordinate c : e) {
 			String key = "Enemy" + Integer.toString(eCount+1);
-			objects.put(key, new Enemy(e[eCount],key));
+			objects.put(key, new Enemy(e[eCount],key,0));
 			grid[c.getX()][c.getY()] = key;
+			eKeys.add(key);
 			eCount++;
 		}
 		int wCount = 0;
@@ -52,6 +56,8 @@ public class Map {
 		default:
 			break;
 		}
+		Coordinate pos = objects.get("player").getPosition();
+		pVisited.add(new int[] {pos.getX(),pos.getY()});
 		move(vector,"player");
 	}
 	
@@ -66,6 +72,50 @@ public class Map {
 		else {
 			System.out.println("Invalid move");
 		}
+	}
+	
+	public void moveEnemies() {
+		//Figure out a target for each of the enemies
+		for(String key : eKeys) {
+			//Get a list of each square the enemies can see
+			Coordinate pos = objects.get(key).getPosition();
+			HashMap<int[], String> fov = new HashMap<int[], String>();
+			//Check each direction in turn
+			fov.putAll(look(pos,true,-1));
+			fov.putAll(look(pos,false,-1));
+			fov.putAll(look(pos,true,1));
+			fov.putAll(look(pos,false,1));
+			
+			//Target the player directly if they can see it
+			//Target the places the player moved this turn
+		}
+	}
+	
+	public HashMap<int[], String> look(Coordinate pos, boolean axis, int dir){
+		HashMap<int[], String> seen = new HashMap<int[], String>();
+		if(axis) { //X axis if true, y axis if false
+			for (int i=pos.getX(); i < grid.length || i > -1; i+=dir) {
+				String current = grid[i][pos.getY()];
+				if (current.contains("Wall")) {
+					break;
+				}
+				else {
+					seen.put(new int[] {i, pos.getY()},current);
+				}
+			}
+		}
+		else {
+			for (int i=pos.getY(); i < grid[0].length || i > -1; i+=dir) {
+				String current = grid[pos.getX()][i];
+				if (current.contains("Wall")) {
+					break;
+				}
+				else {
+					seen.put(new int[] {pos.getX(), i},current);
+				}
+			}
+		}
+		return seen;
 	}
 	
 	public String[][] getGrid(){
--- aitest/actors/Enemy.java
+++ aitest/actors/Enemy.java
@@ -3,8 +3,32 @@ package aitest.actors;
 import aitest.Coordinate;
 
 public class Enemy extends Character{
-	public Enemy(Coordinate c, String name){
+	private Coordinate target;
+	enum Type{
+		RUSH
+	}
+	private Type ai;
+	public Enemy(Coordinate c, String name, int type){
 		super(c);
 		this.setName(name);
+		switch (type){
+		case 0:
+			setAi(Type.RUSH);
+			break;
+		default:
+			break;
+		}
+	}
+	public Coordinate getTarget() {
+		return target;
+	}
+	public void setTarget(Coordinate target) {
+		this.target = target;
+	}
+	public Type getAi() {
+		return ai;
+	}
+	public void setAi(Type ai) {
+		this.ai = ai;
 	}
 }
