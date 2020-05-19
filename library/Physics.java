e6a8c3380446a994aa56397254f524b4d233f8f9
znsolomon <znsolomon@gmail.com>
2020-05-19 12:38:12 +0100

    WIP Enemy AI

--- library/Physics.java
+++ library/Physics.java
@@ -9,4 +9,13 @@ public class Physics {
 			return new int[] {one[0]+two[0],two[1]+one[1]};
 		}
 	}
+	/**
+	 * Calculates the distance between coordinates 'one' and 'two' using pythagoras
+	 * @param one
+	 * @param two
+	 * @return
+	 */
+	public static double calcDistance(int[] one, int[] two) {
+		return Math.sqrt(Math.pow((one[0] - two[0]),2) + Math.pow((one[1] - two[1]),2));
+	}
 }
